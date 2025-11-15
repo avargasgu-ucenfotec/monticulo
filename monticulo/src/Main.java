41124a50c79db4b4f35beda3137f63de35cefa81
avargasgu-ucenfotec <avargasgu@ucenfotec.ac.cr>
2025-11-14 21:32:28 -0600

    Agrega clase MinHeap

--- monticulo/src/Main.java
+++ monticulo/src/Main.java
@@ -1,7 +1,43 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello and welcome!");
+        //Instanciar montículo
+        MinHeap miMonticulo = new MinHeap();
 
+        //Imprimir montículo vacío
+        miMonticulo.imprimirMonticulo();
+
+        //Insertar números enteros al montículo
+        miMonticulo.insertar(10);
+        miMonticulo.insertar(5);
+        miMonticulo.insertar(15);
+        miMonticulo.insertar(3);
+        miMonticulo.insertar(25);
+
+        //Imprimir montículo
+        miMonticulo.imprimirMonticulo();
+
+        //Retornar la raíz del montículo
+        System.out.println(miMonticulo.peek());
+
+        //Imprimir montículo
+        miMonticulo.imprimirMonticulo();
+
+        //Crear un montículo a partir de un arreglo desordenado
+        ArrayList<Integer> arreglo = new ArrayList<>(Arrays.asList(2, 5, 3, 1, 4));
+        MinHeap miNuevoMonticulo = new MinHeap();
+        miNuevoMonticulo.heapify(arreglo);
+
+        //Imprimir montículo
+        miNuevoMonticulo.imprimirMonticulo();
+
+        //Eliminar la raíz de un montículo
+        System.out.println(miMonticulo.eliminarMin());
+
+        //Imprimir montículo
+        miMonticulo.imprimirMonticulo();
     }
 }
---
+++ monticulo/src/MinHeap.java
@@ -0,0 +1,143 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class MinHeap {
+
+    //Atributos
+    private final ArrayList<Integer> heap;
+
+    //Métodos
+    //Constructor
+    public MinHeap() {
+        heap = new ArrayList<>();
+    }
+
+    //Insertar nodo
+    public void insertar(int entero) {
+        //Insertar un número entero al final del arreglo
+        heap.add(entero);
+
+        //Retornar el índice del número entero insertado
+        int indiceNuevo = heap.size() - 1;
+
+        //upHeapify
+        upHeapify(indiceNuevo);
+    }
+
+    //Eliminar raíz
+    public int eliminarMin() {
+        //Revisar si el montículo está vacío
+        if (heap.isEmpty()) {
+            throw new RuntimeException("Operación no válida. Montículo vacío.");
+        }
+
+        //Retornar raíz
+        int min = heap.getFirst();
+
+        //Remover el último número entero en el arreglo
+        int ultimoNumero = heap.removeLast();
+
+        if (!heap.isEmpty()) {
+            // Mover el último número entero a la raíz del montículo
+            heap.set(0, ultimoNumero);
+
+            //downHeapify
+            downHeapify();
+        }
+        return min;
+    }
+
+    //Retornar índice del padre
+    public int indicePadre(int i) {
+        return (i - 1) / 2;
+    }
+
+    //Retorna índice del hijo izquierdo
+    public int indiceHijoIzquierdo(int i) {
+        return 2 * i + 1;
+    }
+
+    //Retorna índice del hijo derecho
+    public int indiceHijoDerecho(int i) {
+        return 2 * i + 2;
+    }
+
+    //Intercambiar los números enteros en los índices i y j
+    private void intercambiar(int i, int j) {
+        int temp = heap.get(i);
+        heap.set(i, heap.get(j));
+        heap.set(j, temp);
+    }
+
+    //Revisa si el montículo está vacío
+    public boolean isEmpty() {
+        return heap.isEmpty();
+    }
+
+    //Intercambiar el nuevo número entero con su padre si su valor es menor que él
+    public void upHeapify(int indiceNuevo) {
+        while (indiceNuevo > 0 && heap.get(indiceNuevo) < heap.get(indicePadre(indiceNuevo))) {
+            intercambiar(indiceNuevo, indicePadre(indiceNuevo));
+            indiceNuevo = indicePadre(indiceNuevo);
+        }
+    }
+
+    //Reordenar el montículo para que la raíz tenga el número entero de menor valor
+    public void downHeapify() {
+        int indiceActual = 0;
+        while (true) {
+            int izquierdo = indiceHijoIzquierdo(indiceActual);
+            int derecho = indiceHijoDerecho(indiceActual);
+
+            int menor = indiceActual;
+
+            if (izquierdo < heap.size() && heap.get(izquierdo) < heap.get(menor)) {
+                menor = izquierdo;
+            }
+
+            if (derecho < heap.size() && heap.get(derecho) < heap.get(menor)) {
+                menor = derecho;
+            }
+
+            if (menor == indiceActual) {
+                break;
+            }
+
+            intercambiar(indiceActual, menor);
+            indiceActual = menor;
+        }
+    }
+
+    //Retornar raíz del montículo
+    public int peek() {
+        return heap.getFirst();
+    }
+
+    //Imprimir elementos del montículo
+    public void imprimirMonticulo() {
+        if (heap.isEmpty()) {
+            System.out.println("Montículo vacío");
+        }
+
+        System.out.print("[ ");
+        for (int e : heap) {
+            System.out.print(e + " ");
+        }
+        System.out.println("]");
+    }
+
+    //Converir un arreglo en un montículo
+    public void heapify(ArrayList<Integer> arreglo) {
+        if (arreglo.isEmpty()) {
+            System.out.println("Arreglo vacío");
+        }
+
+        //Desordenar el arreglo
+        Collections.shuffle(arreglo);
+
+        //Insertar cada elemento del arreglo en el montículo
+        for (int e : arreglo) {
+            insertar(e);
+        }
+    }
+}
